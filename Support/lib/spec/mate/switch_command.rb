a0cf79d4ead903fd1be8b472810ad74262b259b6
John Thornton <ubergeek3141@gmail.com>
2008-05-21 18:40:02 -0600

    Added alternate file support, and RSpec steps grammer

---
+++ Support/lib/spec/mate/switch_command.rb
@@ -0,0 +1,90 @@
+module Spec
+  module Mate
+    # This is based on Ruy Asan's initial code:
+    # http://ruy.ca/posts/6-A-simple-switch-between-source-and-spec-file-command-for-textmate-with-auto-creation-
+    class SwitchCommand
+      def go_to_twin(project_directory, filepath)
+        other = twin(filepath)
+        if File.file?(other)
+          %x{ "$TM_SUPPORT_PATH/bin/mate" "#{other}" }
+        else
+          relative = other[project_directory.length+1..-1]
+          file_type = file_type(other)
+          if create?(relative, file_type)
+            content = content_for(file_type, relative)
+            write_and_open(other, content)
+          end
+        end
+      end
+    
+      def twin(path)
+        if path =~ /^(.*)\/(steps|stories)\/(.*?)$/
+          prefix, parent, rest = $1, $2, $3
+          
+          case parent
+            when 'steps' then
+              path = path.gsub(/\/steps\//, '/stories/')
+              path = path.gsub(/_steps\.rb$/, '.story')
+            when 'stories' then
+              path = path.gsub(/\/stories\/([^\/]*)\.story$/, '/steps/\1_steps.rb')
+          end
+          return path
+        end
+      end
+    
+      def file_type(path)
+        if path =~ /\.story$/
+          return 'story'
+        end
+        if path =~ /_steps\.rb$/
+          return 'steps file'
+        end
+        "file"
+      end
+    
+      def create?(relative_twin, file_type)
+        answer = `#{ ENV['TM_SUPPORT_PATH'] }/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog yesno-msgbox --no-cancel --icon document --informative-text "#{relative_twin}" --text "Create missing #{file_type}?"`
+        answer.to_s.chomp == "1"
+      end
+
+      def content_for(file_type, relative_path)
+        case file_type
+          when /story$/ then
+            story(relative_path)
+          else
+            steps(relative_path)
+        end
+      end
+      
+      # Extracts the snippet text
+      def snippet(snippet_name)
+        snippet_file = File.expand_path(File.dirname(__FILE__) + "/../../../../Snippets/#{snippet_name}")
+        xml = File.open(snippet_file).read
+        xml.match(/<key>content<\/key>\s*<string>([^<]*)<\/string>/m)[1]
+      end
+      
+      def story(path)
+        snippet('Story.tmSnippet')
+      end
+
+      def steps(relative_path)
+        content = <<-STEPS
+steps_for(:${1:steps}) do
+  Given "${2:condition}" do
+
+  end
+end
+STEPS
+      end
+    
+      def write_and_open(path, content)
+        `mkdir -p "#{File.dirname(path)}"`
+        `touch "#{path}"`
+        `osascript &>/dev/null -e 'tell app "SystemUIServer" to activate' -e 'tell app "TextMate" to activate'`
+        `"$TM_SUPPORT_PATH/bin/mate" "#{path}"`
+        escaped_content = content.gsub("\n","\\n").gsub('$','\\$').gsub('"','\\\\\\\\\\\\"')
+        `osascript &>/dev/null -e "tell app \\"TextMate\\" to insert \\"#{escaped_content}\\" as snippet true"`      
+      end
+    end
+  end
+end
---
+++ Support/spec/spec/mate/switch_command_spec.rb
@@ -0,0 +1,110 @@
+require File.dirname(__FILE__) + '/../../../lib/spec/mate/switch_command'
+
+module Spec
+  module Mate
+    class Twin
+      def initialize(expected)
+        @expected = expected
+      end
+
+      def matches?(actual)
+        @actual = actual
+        # Satisfy expectation here. Return false or raise an error if it's not met.
+        command = SwitchCommand.new
+
+        path = command.twin(@actual)
+        path.should == @expected
+
+        path = command.twin(@expected)
+        path.should == @actual
+        true
+      end
+
+      def failure_message
+        "expected #{@actual.inspect} to twin #{@expected.inspect}, but it didn't"
+      end
+
+      def negative_failure_message
+        "expected #{@actual.inspect} not to twin #{@expected.inspect}, but it did"
+      end
+      
+      def description
+        "twin #{@actual} <=> #{@expected}"
+      end
+    end
+    
+    class BeA
+      def initialize(expected)
+        @expected = expected
+      end
+
+      def matches?(actual)
+        @actual = actual
+        c = SwitchCommand.new
+        c.file_type(@actual).should == @expected
+        true
+      end
+
+      def failure_message
+        "expected #{@actual.inspect} to be_a #{@expected.inspect}, but it didn't"
+      end
+
+      def negative_failure_message
+        "expected #{@actual.inspect} not to be_a #{@expected.inspect}, but it did"
+      end
+    end
+
+    def be_a(expected)
+      BeA.new(expected)
+    end
+
+    describe SwitchCommand, "in a regular app" do
+      include Spec::Mate
+      def twin(expected)
+        Twin.new(expected, false)
+      end
+      
+      it do
+        "/a/full/path/stories/snoopy/stories/mooky.story".should twin(
+        "/a/full/path/stories/snoopy/steps/mooky_steps.rb")
+      end
+      
+      it "should suggest plain story" do
+        "/a/full/path/stories/snoopy/stories/mooky.story".should be_a("story")
+      end
+
+      it "should suggest plain step file" do
+        "/a/full/path/stories/snoopy/steps/mooky_steps.rb".should be_a("steps file")
+      end
+
+      it "should create story for story files" do
+        story_file = <<-STORY
+Story: title
+
+  As a role
+  I want feature
+  So that value
+
+  Scenario: title
+    Given context
+    When event
+    Then outcome
+STORY
+        SwitchCommand.new.content_for('story', "stories/stories/basic.story").should == story_file
+        SwitchCommand.new.content_for('story', "stories/foo/stories/basic.story").should == story_file
+      end
+      
+      it "should create steps for steps files" do
+        steps_file = <<-STEPS
+steps_for(:${1:steps}) do
+  Given "${2:condition}" do
+
+  end
+end
+STEPS
+        SwitchCommand.new.content_for('spec', "spec/steps/basic_steps.rb").should == steps_file
+        SwitchCommand.new.content_for('spec', "spec/foo/steps/basic_steps.rb").should == steps_file
+      end
+    end
+  end
+end
