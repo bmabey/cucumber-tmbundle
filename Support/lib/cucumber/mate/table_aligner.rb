06f642c011517de5832f7488a3c7d22ada6c862e
Darrin Holst <darrinholst@gmail.com>
2009-09-27 20:51:56 -0500

    handle multiple tables in the TableAligner

--- Support/lib/cucumber/mate/table_aligner.rb
+++ Support/lib/cucumber/mate/table_aligner.rb
@@ -2,22 +2,50 @@ module Cucumber
   module Mate
     class TableAligner
       def align(table_text)
-        table_text =~ /^(\s*)\|.*$/
-        initial_space = $1 
+        align_all(table_text.split("\n")).join("\n")
+      end
 
-        table_data = table_text.split(/\n/).map do |line|
-          just_cells = line.strip  
-          just_cells.split('|').map{|cell| cell.strip }
+      private
+
+      def align_all(lines)
+        group_by_tables(lines).map do |group|
+          if(group.is_a? Array)
+            align_table(group)
+          else
+            group
+          end
+        end
+      end
+
+      def group_by_tables(lines)
+        current_table = []
+        grouped = []
+
+        lines.each do |line|
+          if line.match(/\s*\|/)
+            current_table << line
+          else
+            grouped << current_table unless current_table.empty?
+            grouped << line
+            current_table = []
+          end
         end
 
-        max_lengths = table_data.transpose.map { |col| col.map { |cell| cell.unpack("U*").length }.max }.flatten
+        grouped << current_table unless current_table.empty?
+        grouped
+      end
+
+      def align_table(table)
+        table_data = table.map{|line| line.strip.split('|').map{|cell| cell.strip}}
+        max_lengths =  table_data.transpose.map { |col| col.map { |cell| cell.unpack("U*").length }.max }.flatten
+        initial_space = table.first.match(/(\s*)|/)[1]
 
         table_data.map do |line|
           initial_space[0..-2].to_s + \
           line.zip(max_lengths).map { |cell, max_length|
             "%-#{ max_length }s" % cell
-          } .join(' | ') + ' |'
-        end.join("\n")
+          }.join(' | ') + ' |'
+        end
       end
     end
   end
--- Support/spec/cucumber/mate/table_aligner_spec.rb
+++ Support/spec/cucumber/mate/table_aligner_spec.rb
@@ -5,10 +5,31 @@ module Cucumber
   module Mate
     describe TableAligner do
       it "should align a simple table" do
-        unaligned = "   |    a |b|\n|c|  d   |"
-        TableAligner.new.align(unaligned).should == "   | a | b |\n   | c | d |"
+        unaligned = "  |    a |b|\n" +
+                    " |c|  d   |"
+
+        expected = "  | a | b |\n" +
+                   "  | c | d |"
+
+        TableAligner.new.align(unaligned).should == expected
       end
-      
+
+      it "should align multiple tables" do
+        unaligned = "  |    a |b|\n" +
+                    " |c|  d   |\n" +
+                    "\n" +
+                    "   |x  | y|zz|\n" +
+                    " |1|2|3|"
+
+        expected = "  | a | b |\n" +
+                   "  | c | d |\n" +
+                   "\n" +
+                   "   | x | y | zz |\n" +
+                   "   | 1 | 2 | 3  |"
+
+        TableAligner.new.align(unaligned).should == expected
+      end
+
       it "should align a table with multi-byte UTF8 values" do
         unaligned = "   | a |b|\n" +
                     "   |÷|  d  |"
